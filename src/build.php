c59ff43822ba3ed4a17448bd9aa5004bb36de25c
Tim Nolte <tim.nolte@ndigitals.com>
2022-05-09 22:17:41 -0400

    Initial Setup Based on `wp-phpunit/wp-phpunit`

---
+++ src/build.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Build;
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use PHPGit\Git;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+$app = new Application();
+$app->instance('path.root', dirname(__DIR__));
+$app->instance('path.artifacts', "{$app['path.root']}/artifacts");
+$app->instance('path.templates', "{$app['path.root']}/template");
+$app->instance('path.repo.woocommerce', "{$app['path.root']}/repos/woocommerce");
+$app->instance('path.repo.package', "{$app['path.root']}/repos/package");
+
+$app->singleton('git.woocommerce', function ($app) {
+    return tap(new Git, function (Git $git) use ($app) {
+      $git->setRepository($app['path.repo.woocommerce']);
+    });
+});
+$app->singleton('git.package', function ($app) {
+    return tap(new Git, function (Git $git) use ($app) {
+        $git->setRepository($app['path.repo.package']);
+    });
+});
+$app->singleton('logger', function () {
+    return tap(new Logger('build'), function (Logger $logger) {
+        $logger->pushHandler(new StreamHandler(fopen('php://stdout', 'w')));
+    });
+});
+
+$app->run();
---
+++ template/README.md
@@ -0,0 +1,11 @@
+# WC PHPUnit
+
+[![Packagist Version](https://img.shields.io/packagist/v/ndigitals/wc-phpunit.svg)](https://packagist.org/packages/ndigitals/wc-phpunit)
+[![Packagist Downloads](https://img.shields.io/packagist/dt/ndigitals/wc-phpunit.svg)](https://packagist.org/packages/ndigitals/wc-phpunit)
+[![Packagist License](https://img.shields.io/packagist/l/ndigitals/wc-phpunit.svg)](https://packagist.org/packages/ndigitals/wc-phpunit)
+
+The WooCommerce PHPUnit library.
+
+**This repository is built from the official WooCommerce git repository `https://github.com/woocommerce/woocommerce/tree/trunk/plugins/woocommerce`. Please direct all issues [here](https://github.com/ndigitals/wc-phpunit-build/issues).**
+
+For more information, [see the documentation](https://github.com/ndigitals/wc-phpunit/wiki).
